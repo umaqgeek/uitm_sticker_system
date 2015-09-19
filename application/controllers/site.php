6024f0073fdf8e4f7ab2404bb5f1d615a177e898
rabani92 <rabanirosli@yahoo.com>
2015-09-19 11:19:50 +0800

    tambah crud

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -3,10 +3,10 @@
 class Site extends MY_Controller 
 {
         var $parent_page = "site";
-	function __construct()
-	{
+    function __construct()
+    {
             parent::__construct(); 
-	}
+    }
 
 
     private function viewpage($page='v_mainpage', $data=array())
@@ -16,12 +16,36 @@ class Site extends MY_Controller
             echo $this->load->view($this->parent_page.'/'.$page, $data, true);
             echo $this->load->view('v_footer', $data, true);
         }
+
+        public function signup1()
+        {
+            try{
+                $crud = new grocery_CRUD();
+
+                $crud->set_theme('sayapunyer');
+                $crud->set_table('registration');
+                $crud->required_fields('plat', 'kenderaan');
+                $crud->columns('plat', 'kenderaan');
+                $crud->add_fields('plat','kenderaan');
+                $crud->edit_fields('kenderaan');
+                $crud->unset_add();
+                $crud->unset_delete();
+                $crud->unset_edit();
+
+                $output = $crud->render();
+
+                $this->viewpage('v_crud', $output);
+
+            }catch(Exception $e){
+                show_error($e->getMessage().' --- '.$e->getTraceAsString());
+            }
+        }
         
         
 
 
         public function index()
-	{
+    {
             $this->load->model('m_signup');
             $data['signup'] = $this->m_signup->get();
             $this->load->view('login/signup', $data);
